76fb5323e2d9b013954370c521b33688dd469645
pravyada <pravesh.yadav@capgemini.com>
2016-09-08 18:44:44 +0530

    code commited

--- src/main/java/domain/Dog.java
+++ src/main/java/domain/Dog.java
@@ -4,4 +4,45 @@ package domain;
  * Created by pravyada on 9/8/2016.
  */
 public class Dog {
+    private final String name;
+    private final String breed;
+    private final String color;
+
+
+    public Dog(String name, String breed, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+    }
+
+    public static DogBuilder called(String name) {
+        return new DogBuilder(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public static class DogBuilder {
+        private final String name;
+        private String breed;
+
+        public DogBuilder(String name) {
+            this.name = name;
+        }
+
+
+        public DogBuilder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog andOfColor(String color) {
+            return new Dog(name,breed,color);
+        }
+    }
 }
--- src/test/java/domain/WhenCreatingANewDog.java
+++ src/test/java/domain/WhenCreatingANewDog.java
@@ -1,7 +1,18 @@
 package domain;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by pravyada on 9/8/2016.
  */
 public class WhenCreatingANewDog {
+
+    @Test
+    public void a_dog_should_have_a_name(){
+        Dog fido = Dog.called("Fido").ofBreed("Laborador").andOfColor("Black");
+
+        Assert.assertEquals(fido.getName(),"Fido");
+        Assert.assertEquals(fido.getBreed(),"Laborador");
+    }
 }
